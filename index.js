41d76bb05268f61cd5f1603e64145f2c3d3faa23
Danique Lammertink <danique.lammertink@hva.nl>
2022-03-04 11:26:18 +0100

    update

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const { engine } = require('express-handlebars');
+const app = express();
+
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.engine('.hbs', engine({
+  extname: '.hbs',
+  defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
+app.set("views", "./views");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('home');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000);
